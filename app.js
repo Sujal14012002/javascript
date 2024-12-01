41cb7a50ff65b05bc893df91e80ef54451f7db6d
Sujal Prajapati <prajapatisujal75@gmail.com>
2024-12-01 12:40:54 -0800

    function-code

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-var x=1;
-a();
-b();
-console.log(x);
-
-
 function a(){
-    var x=10;
-    console.log(x);
+c();
+function c(){
+    console.log(b);
+}    
 }
-
-function b(){
-    var x=100;
-    console.log(x);
-}
-
-
-
-
-
-
+var b=10;
+a();
