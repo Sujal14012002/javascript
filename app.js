f219bf001eec82c95d8c8ce7be05abfa2ae8d7ef
Sujal Prajapati <prajapatisujal75@gmail.com>
2024-12-05 01:05:53 -0800

    condition-operator

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-let a=44.2;
-let b=45.1;
-console.log(Math.trunc(a+b));
+const age=45;
+if(age>=47){
+    console.log("code")
+}
+else if(age>=41){
+    console.log("java");
+}
+else{
+    console.log("hello");
+}
