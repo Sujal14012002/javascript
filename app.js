3de0725f6e3e04ce2a25cf790e22b9ebc7b7f3c0
Sujal Prajapati <prajapatisujal75@gmail.com>
2024-12-05 01:47:57 -0800

    assigned-operator

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-const option = 8;
-switch (option) {
-  case 1:
+ // logical operator
+
+ //and
+ const age =25;
+
+ if(age>=18 || age<=16){
     console.log("hello");
-    break;
-  case 2:
-    console.log("priyam");
-    break;
-  case 3:
-    console.log("ysh");
-    break;
-  case 4:
-    console.log("kmfkm");
-    break;
-
-  case 5:
-    console.log("phbg");
-    break;
-
-  case 6:
-    console.log("on");
-    break;
-    default:
-        console.log("higher order");
-
-}
+ }
