ea6b9d05130f7a85d888357f79ee3219a4fb2254
Sujal Prajapati <prajapatisujal75@gmail.com>
2024-12-05 01:12:51 -0800

    ternary-operator

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const age=45;
-if(age>=47){
-    console.log("code")
-}
-else if(age>=41){
-    console.log("java");
-}
-else{
-    console.log("hello");
-}
+// const age=45;
+// if(age>=47){
+//     console.log("code")
+// }
+// else if(age>=41){
+//     console.log("java");
+// }
+// else{
+//     console.log("hello");
+// }
+
+// ternary operator
+const age =18;
+age>18 ? console.log("adult"):console.log("teen-age");
